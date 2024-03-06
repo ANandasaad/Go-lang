283aa92cfaf610f164263955e86f5c17f65c55fa
Anand kushwaha <anandkushwaha@Anands-Mac-mini.local>
2024-03-06 15:16:15 +0530

    buildapi

--- buildapi/main.go
+++ buildapi/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -23,10 +27,27 @@ var courses []Course
 
 // middleware , helper function
 func (c *Course) IsEmpty() bool {
-	return c.CourseId == "" && c.CourseName == ""
+	// return c.CourseId == "" && c.CourseName == ""
+	return c.CourseName == ""
 }
 func main() {
+	fmt.Println("API Documentation")
+	r := mux.NewRouter()
 
+	//seeding
+	courses = append(courses, Course{CourseId: "1", CourseName: "React js", CoursePrice: 500, Author: &Author{FullName: "Anand Kushwaha", Website: "http://Reactjs.com"}})
+	courses = append(courses, Course{CourseId: "2", CourseName: "Next js", CoursePrice: 500, Author: &Author{FullName: "Anand Kushwaha", Website: "http://Nextjs.com"}})
+	courses = append(courses, Course{CourseId: "3", CourseName: "Node js", CoursePrice: 500, Author: &Author{FullName: "Anand Kushwaha", Website: "http://Nodejs.com"}})
+
+	r.HandleFunc("/", serveHome).Methods("GET")
+	r.HandleFunc("/courses", getAllCourse).Methods("GET")
+	r.HandleFunc("/course/{id}", getOneCourse).Methods("GET")
+	r.HandleFunc("/create-course", createOneCourse).Methods("POST")
+	r.HandleFunc("/update-course/{id}", updateOneCourse).Methods("PUT")
+	r.HandleFunc("/delete-course/{id}", deleteOneCourse).Methods("DELETE")
+
+	//listen to a port
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 // controller -file
@@ -61,4 +82,67 @@ func getOneCourse(w http.ResponseWriter, r *http.Request) {
 
 	}
 	json.NewEncoder(w).Encode("No courses found")
+	return
+}
+
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data to the server")
+	}
+	// what about ={}
+
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if course.IsEmpty() {
+		json.NewEncoder(w).Encode("No data found")
+		return
+	}
+
+	// Generate unique id , convert into string
+	// append course into courses
+
+	rand.Seed(time.Now().UnixNano())
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
+	return
+}
+
+func updateOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update Course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// first - id
+	params := mux.Vars(r)
+
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			var course Course
+			_ = json.NewDecoder(r.Body).Decode(&course)
+			course.CourseId = params["id"]
+			courses = append(courses, course)
+			json.NewEncoder(w).Encode(course)
+			return
+
+		}
+	}
+
+}
+
+func deleteOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("delete one course")
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			break
+		}
+
+	}
+	json.NewEncoder(w).Encode("Successfully deleted")
 }
